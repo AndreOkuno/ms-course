6d04b493f49c8b7c1725dfe8ee5fdddc70657c39
andre <andre_okuno@hotmail.com>
2021-08-19 13:29:26 -0300

    Fixed everthing - Now the program is run Ok

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
+/*classe que gerencia e roteia as chamadas a partir dos nomes do ms*/
 
 @EnableEurekaClient
 @EnableZuulProxy
